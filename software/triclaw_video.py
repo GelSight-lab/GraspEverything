3cedfc0edf442e047614a7eb30847d9d5319a9da
Mike Burgess <39389326+mburgjr@users.noreply.github.com>
2025-01-20 19:20:48 -0500

    Update triclaw_video.py

--- software/triclaw_video.py
+++ software/triclaw_video.py
@@ -23,24 +23,14 @@ def trim(img):
     img[img > 255] = 255
 
 # Measured from calipers on surface in x and y directions
-WARPED_PX_TO_MM = (11, 11)
-RAW_PX_TO_MM = (12.5, 11)
-PX_TO_MM = np.sqrt(WARPED_PX_TO_MM[0]**2 + WARPED_PX_TO_MM[1]**2)
+PX_TO_MM = 16.6
 
 # Derived from linear fit from max depth measured to known calibration ball diameter
-DEPTH_TO_MM = 21.5
+DEPTH_TO_MM = 13.75
 
 # Threshold which is considered more than noise (significant penetration)
 DEPTH_THRESHOLD = 0.1 # 0.03 # [mm]
 AUTO_CLIP_OFFSET = 10 # [frames]
-
-# Constants for cropping the edges of images
-EDGE_CROP_MARGIN = 25 # [pixels]
-
-# Size before and after warping + cropping
-ORIGINAL_IMG_SIZE = (480, 640)
-WARPED_IMG_SIZE = (400, 300)
-WARPED_CROPPED_IMG_SIZE = (400 - EDGE_CROP_MARGIN*2, 300 - EDGE_CROP_MARGIN*2)
 
 # Streaming parameters
 STREAM_FPS = 30.0 # [1/s]
@@ -49,20 +39,19 @@ class GSTriClawVideo():
     '''
     Class to streamline processing of video data from TriClaw
     '''
-    def __init__(self, config_csv="./config.csv", IP=None, markers=False, warped_size=WARPED_IMG_SIZE, edge_crop_margin=EDGE_CROP_MARGIN):
+    def __init__(self, config_csv="./config.csv", IP=None, markers=False):
         self.corners = read_csv(config_csv)         # CSV with pixel coordinates of mirror corners in the order (topleft,topright,bottomleft,bottomright)
-        self.image_size = ORIGINAL_IMG_SIZE         # The size of original image from camera
-        self.warped_size = warped_size              # The size of the image to output from warping process
-        self.edge_crop_margin = edge_crop_margin    # Number of pixels to crop off edge after warping
         self.FPS = STREAM_FPS                       # Default FPS from Raspberry Pi camera
-        self.PX_TO_MM = PX_TO_MM                    # Conversion from pixels to mm
 
         # If markers are being used, we will demark the image
         self.markers = markers
 
-        # Compute size of the image after warping and edges are cropped
-        self.cropped_size = (warped_size[0] - 2*self.edge_crop_margin, \
-                             warped_size[1] - 2*self.edge_crop_margin)
+        # Choose size to crop image to
+        self.cropped_size = (1200, 600)
+
+        # Create tactile sensing mask based on config
+        mask = np.zeros((self.cropped_size[1], self.cropped_size[0]), dtype=np.uint8)
+        self.tactile_mask = cv2.fillPoly(mask, [np.array(self.corners)], 1)
 
         self._IP = IP                       # IP address of Raspberry Pi stream via mjpg_streamer
         self._url = ''                      # URL address of Raspberry Pi stream via mjpg_streamer
@@ -77,17 +66,15 @@ class GSTriClawVideo():
         self._stream_active = False         # Boolean of whether or not we're currently streaming
         self._plotting = False              # Boolean of whether or not we're plotting during stream
 
-        self._raw_rgb_frames = []           # List of raw RGB frames recorded from camera (in shape: [(640,480,3)])
-        self._warped_rgb_frames = []        # Images from camera now cropped / warped to the mirror (in shape: [(640,480,3)])
-        self._diff_images = []              # Difference images relative to first frame (after warping) (in shape: [(640,480,3)])
-        self._grad_images = []              # Gradient images estimated from RGB (in shape: [(640,480,2)])
-        self._depth_images = []             # Depth images calculated (in shape: [(640,480)])
+        self._raw_rgb_frames = []           # List of raw RGB frames recorded from camera (in shape: (W,H,3))
+        self._diff_images = []              # Difference images relative to first frame (in shape: (W,H,3))
+        self._grad_images = []              # Gradient images estimated from RGB (in shape: (W,H,2))
+        self._depth_images = []             # Depth images calculated (in shape: (W,H))
     
     # Clear all video data from the object
     def _reset_frames(self):
         self._curr_rgb_image = None
         self._raw_rgb_frames = []
-        self._warped_rgb_frames = []
         self._diff_images = []
         self._grad_images = []
         self._depth_images = []
@@ -96,22 +83,13 @@ class GSTriClawVideo():
     # Return stored raw frames from Raspberry Pi camera
     def raw_RGB_frames(self):
         return np.stack(self._raw_rgb_frames, axis=0)
-    
-    # Return cropped and warped images, compute if necessary
-    def warped_RGB_frames(self):
-        if len(self._warped_rgb_frames) != len(self._raw_rgb_frames):
-            self._warped_rgb_frames = []
-            for img in self.raw_RGB_frames():
-                self._warped_rgb_frames.append(self.crop_image(self.warp_image(img)))
-        return np.stack(self._warped_rgb_frames, axis=0)
 
-    # Return cropped and warped difference images from initial frame to rest of video
+    # Return difference images from initial frame to rest of video
     def diff_images(self):
         if len(self._diff_images) != len(self._raw_rgb_frames):
             self._diff_images = []
-            ref_img = self.warped_RGB_frames()[0]
-            # ref_img = cv2.GaussianBlur(ref_img, (13, 13), 0)
-            for img in self.warped_RGB_frames():
+            ref_img = self.raw_rgb_frames()[0]
+            for img in self.raw_rgb_frames():
                 self._diff_images.append(self.calc_diff_image(ref_img, img))
         return np.stack(self._diff_images, axis=0)
     
@@ -143,14 +121,6 @@ class GSTriClawVideo():
     def mean_depths(self):
         return np.mean(self.depth_images(), axis=(1,2))
     
-    # Warp raw image to mirror shape based on config corners
-    def warp_image(self, img):
-        return warp_perspective(img, self.corners, self.warped_size)
-    
-    # Crop edges of the image to avoid inaccuracies
-    def crop_image(self, img):
-        return img[EDGE_CROP_MARGIN:img.shape[0]-EDGE_CROP_MARGIN, EDGE_CROP_MARGIN:img.shape[1]-EDGE_CROP_MARGIN]
-    
     # Calculate difference image from reference frame
     def calc_diff_image(self, ref_img, img):
         return (img * 1.0 - cv2.GaussianBlur(ref_img, (11, 11), 0) * 1.0) / 255 + 0.5
@@ -162,10 +132,15 @@ class GSTriClawVideo():
         dy = interpolate_grad(dy, mask)
         return dx, dy
 
-    # Calculate gradients from a cropped / warped difference image
+    # Calculate gradients from difference image
     def img2grad(self, diff_img):
-        dx = (diff_img[:, :, 1] - (diff_img[:, :, 0] + diff_img[:, :, 2]) * 0.5)
-        dy = (diff_img[:, :, 0] - diff_img[:, :, 2])
+        # Apply polygon based on configuration
+        tactile_diff_img = np.copy(diff_img)
+        tactile_diff_img[self.tactile_mask == 0] = 0.5
+
+        # Compute diff
+        dx = (tactile_diff_img[:, :, 0] - (tactile_diff_img[:, :, 2] + tactile_diff_img[:, :, 1]) * 0.5)
+        dy = (tactile_diff_img[:, :, 2] - tactile_diff_img[:, :, 1])
         dx = dx / (1 - dx ** 2) ** 0.5 / 128
         dy = dy / (1 - dy ** 2) ** 0.5 / 128
         return dx, dy
@@ -179,7 +154,7 @@ class GSTriClawVideo():
         depth_in_mm = DEPTH_TO_MM * unitless_depth # Derived from linear fit of ball calibration
         return depth_in_mm
 
-    # Calculate depth from a cropped / warped difference image
+    # Calculate depth from difference image
     def img2depth(self, diff_img):
         dx, dy = self.img2grad(diff_img)
         depth = self.grad2depth(diff_img, dx, dy)
@@ -225,13 +200,15 @@ class GSTriClawVideo():
     
     # During streaming, read object from URL
     def _decode_image_from_stream(self):
-        self._bytes += self._url_stream.read(32767)
+        self._bytes += self._url_stream.read(32768)
         a = self._bytes.find(b'\xff\xd8')
         b = self._bytes.find(b'\xff\xd9')
         if a != -1 and b != -1:
             jpg = self._bytes[a:b+2]
             self._bytes = self._bytes[b+2:]
-            self._curr_rgb_image = cv2.imdecode(np.frombuffer(jpg, dtype=np.uint8), cv2.IMREAD_COLOR)
+            self._curr_rgb_image = cv2.resize(
+                cv2.imdecode(np.frombuffer(jpg, dtype=np.uint8), cv2.IMREAD_COLOR), self.cropped_size
+            )
             self._raw_rgb_frames.append(self._curr_rgb_image)
             return True # Return whether or not an image was saved
         return False
@@ -253,7 +230,7 @@ class GSTriClawVideo():
             cv2.imshow('raw_RGB', self._curr_rgb_image)
 
             if plot_diff or plot_depth:
-                diff_img = self.calc_diff_image(self.crop_image(self.warp_image(self._raw_rgb_frames[0])), self.crop_image(self.warp_image(self._curr_rgb_image)))
+                diff_img = self.calc_diff_image(self._raw_rgb_frames[0], self._curr_rgb_image)
 
             # Plot difference image
             if plot_diff:
@@ -301,8 +278,6 @@ class GSTriClawVideo():
         i_end = min(i_end, len(self._raw_rgb_frames))
 
         # Clip all other frame data, if applicable
-        if len(self._warped_rgb_frames) == len(self._raw_rgb_frames):
-            self._warped_rgb_frames = self._warped_rgb_frames[i_start:i_end]
         if len(self._diff_images) == len(self._raw_rgb_frames):
             self._diff_images = self._diff_images[i_start:i_end]
         if len(self._grad_images) == len(self._raw_rgb_frames):
@@ -372,7 +347,7 @@ class GSTriClawVideo():
     # Write recorded frames to video file
     def save(self, path_to_file):
         fourcc = cv2.VideoWriter_fourcc(*'MJPG')
-        video_writer = cv2.VideoWriter(path_to_file, fourcc, self.FPS, (self.image_size[1], self.image_size[0]))
+        video_writer = cv2.VideoWriter(path_to_file, fourcc, self.FPS, (self.cropped_size[1], self.cropped_size[0]))
         for frame in self._raw_rgb_frames:
             video_writer.write(frame)
         video_writer.release()
@@ -380,8 +355,9 @@ class GSTriClawVideo():
     
 
 if __name__ == "__main__":
-    tri_claw_video = GSTriClawVideo(IP="128.31.38.56", config_csv="./config/config_markers.csv")
-    tri_claw_video.start_stream(plot=True, plot_diff=True)
-    time.sleep(30)
+    tri_claw_video = GSTriClawVideo(IP="128.31.38.56", config_csv="config.csv")
+    tri_claw_video.start_stream(plot=True, plot_diff=True, plot_depth=True)
+    _ = input("Press enter to stop recording...")
+    print('Recording stopped.')
     tri_claw_video.end_stream()
     tri_claw_video.save('./test.avi')
